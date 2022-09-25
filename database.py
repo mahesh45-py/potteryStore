cd3a6ce731153e5c70bba9209738d8a3136ec851
Mahesh Srikakulam <srikakulammahesh2085@gmail.com>
2022-09-25 16:02:20 +0530

    url changes

--- database.py
+++ database.py
@@ -4,5 +4,5 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 migrate = Migrate()
 # baseUrl = "http://localhost:8080"
-baseUrl = "http://192.168.41.230:5000"
-# baseUrl = "https://mahesh54.pythonanywhere.com"
+# baseUrl = "http://192.168.41.230:5000"
+baseUrl = "https://mahesh54.pythonanywhere.com"
--- manage.py
+++ manage.py
@@ -102,4 +102,4 @@ def method_name():
 
 # app.run(debug=True,port=8080)
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=False,host='0.0.0.0')
