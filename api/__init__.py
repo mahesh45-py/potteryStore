08f636c21f99e185c2a39e411fc8b6ae2c95098e
Mahesh Srikakulam <srikakulammahesh2085@gmail.com>
2022-09-27 01:36:55 +0530

    done changes

--- api/__init__.py
+++ api/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask_restful import Resource, Api
 from controllers.products_controller import ProductsController
 from controllers.products_categories_controller import ProductsCategoriesController
+from controllers.banners_controller import BannersController
 api_bp = Blueprint('api',__name__,url_prefix='/api/<version>')
 
 api = Api(api_bp)
@@ -12,3 +13,4 @@ def index(version):
     
 api.add_resource(ProductsController,'/products')
 api.add_resource(ProductsCategoriesController,'/productCategories')
+api.add_resource(BannersController,'/banners')
